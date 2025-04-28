9da0856de631ae7d3426ebd1f2a8ce286681c8cd
dmoralesitb <diego.morales.7e8@itb.cat>
2025-04-28 11:52:52 +0200

    first

--- app/src/main/java/com/example/mapsapp/ui/navigation/NavigatonWrapper.kt
+++ app/src/main/java/com/example/mapsapp/ui/navigation/NavigatonWrapper.kt
@@ -1,2 +1,10 @@
 package com.example.mapsapp.ui.navigation
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    NavHost
+
+}
