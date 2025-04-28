187db6067e0cf772e20efa191d4b7c50917ef3fb
dmoralesitb <diego.morales.7e8@itb.cat>
2025-04-28 12:05:38 +0200

    first

--- app/src/main/java/com/example/mapsapp/ui/navigation/NavigatonWrapper.kt
+++ app/src/main/java/com/example/mapsapp/ui/navigation/NavigatonWrapper.kt
@@ -1,10 +1,24 @@
 package com.example.mapsapp.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
 
 @Composable
-fun Navigation(){
-    val navController = rememberNavController()
-    NavHost
+fun Navigation(navController : NavHostController){
+    NavHost(navController = navController, startDestination = "MapScreen"){
+        composable<Pantalla1>{
+            MapScreen{navController.navigateTo()}
+        }
+        composable("CreateMarkerScreen"){
+            CreateMarkerScreen()
+            }
+        composable("DetailMarkerScreen"){
+            DetailMarkerScreen()
+        }
+        composable("PermissionsScreen"){
+    }
 
 }
--- app/src/main/java/com/example/mapsapp/ui/navigation/Routes.kt
+++ app/src/main/java/com/example/mapsapp/ui/navigation/Routes.kt
@@ -1,2 +1,12 @@
 package com.example.mapsapp.ui.navigation
 
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+
+object Pantalla1
+
+object pantalla2
+
+object oantalla3
