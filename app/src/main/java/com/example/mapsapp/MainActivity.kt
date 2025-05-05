34902fd319c8003683795d887892f14c046d870e
dmoralesitb <diego.morales.7e8@itb.cat>
2025-05-05 12:39:19 +0200

    first

--- app/src/main/java/com/example/mapsapp/MainActivity.kt
+++ app/src/main/java/com/example/mapsapp/MainActivity.kt
@@ -7,23 +7,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mapsapp.ui.screens.MyDrawerMenu
+import com.example.mapsapp.ui.theme.MapsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyDrawerMenu()
+            MapsAppTheme {
+                MyDrawerMenu()
+            }
 
         }
     }
 }
 
 
-
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-    MyDrawerMenu()
+
+    MapsAppTheme {
+        MyDrawerMenu()
+    }
 }
 
