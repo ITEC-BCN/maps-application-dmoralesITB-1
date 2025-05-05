5f4527e51a02749cfcfdb8b376f4cd74c277f60a
dmoralesitb <diego.morales.7e8@itb.cat>
2025-05-05 12:17:48 +0200

    first

--- app/src/main/java/com/example/mapsapp/ui/screens/MapScreen.kt
+++ app/src/main/java/com/example/mapsapp/ui/screens/MapScreen.kt
@@ -2,9 +2,18 @@ package com.example.mapsapp.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -12,11 +21,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.navigation.compose.rememberNavController
+import com.example.mapsapp.ui.navigation.DrawerItem
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.example.mapsapp.ui.navigation.NavigationWrapper
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
+import kotlinx.coroutines.launch
 
 @Composable
 fun MapsScreen(modifier: Modifier = Modifier, navigateTo: () -> Unit) {
@@ -32,13 +47,55 @@ fun MapsScreen(modifier: Modifier = Modifier, navigateTo: () -> Unit) {
     }
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyDrawerMenu(){
-    val navController= rememberNavController()
-    val drawerState= rememberDrawerState(DrawerValue.Closed)
+fun MyDrawerMenu() {
+    val navController = rememberNavController()
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     val selectedItemIndex by remember { mutableIntStateOf(0) }
-    ModalNavigationDrawer()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = false,
+        drawerContent = {
+            ModalDrawerSheet {
+                DrawerItem.entries.forEachIndexed { index, drawerItem ->
+                    NavigationDrawerItem(
+                        icon = {
+                            Icon(
+                                imageVector = drawerItem.icon,
+                                contentDescription = drawerItem.text
+                            )
+                        },
+                        label = { Text(text = drawerItem.text) },
+                        selected = index == selectedItemIndex,
+                        onClick = {
+                            //selectedItemIndex = index // This line is not needed here
+                            scope.launch { drawerState.close() }
+                            navController.navigate(drawerItem.ruta)
+                        }
+                    )
+                }
+            }
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Awesome App") },
+                    navigationIcon = {
+                        IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                            Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            NavigationWrapper(navController, Modifier.padding(innerPadding))
+        }
+
+
+    }
 
 }
